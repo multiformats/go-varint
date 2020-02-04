0aa688902217dff2cba0f678c7e4a0f547b4983e
Steven Allen <steven@stebalien.com>
2020-02-03 20:46:39 -0800

    fix: varints are LSB
    Got this completely wrong. To check for minimal encoding, we need to verify
    that either:
    
    1. There is exactly one 0.
    2. The MSB of the varint isn't a 0.

--- varint_test.go
+++ varint_test.go
@@ -15,6 +15,16 @@ func checkVarint(t *testing.T, x uint64) {
 	if size != expected {
 		t.Fatalf("expected varintsize of %d to be %d, got %d", x, expected, size)
 	}
+	xi, n, err := FromUvarint(buf)
+	if err != nil {
+		t.Fatal("decoding error", err)
+	}
+	if n != size {
+		t.Fatal("read the wrong size")
+	}
+	if xi != x {
+		t.Fatal("expected a different result")
+	}
 }
 
 func TestVarintSize(t *testing.T) {
@@ -44,7 +54,8 @@ func TestOverflow(t *testing.T) {
 }
 
 func TestNotMinimal(t *testing.T) {
-	i, n, err := FromUvarint([]byte{0x80, 0x01})
+	varint := []byte{0x81, 0x00}
+	i, n, err := FromUvarint(varint)
 	if err != ErrNotMinimal {
 		t.Error("expected an error")
 	}
@@ -53,6 +64,29 @@ func TestNotMinimal(t *testing.T) {
 	}
 	if i != 0 {
 		t.Error("expected i = 0")
+	}
+	i, n = binary.Uvarint(varint)
+	if n != len(varint) {
+		t.Error("expected to read entire buffer")
+	}
+	if i != 1 {
+		t.Error("expected varint 1")
+	}
+}
+
+func TestNotMinimalRead(t *testing.T) {
+	varint := bytes.NewBuffer([]byte{0x81, 0x00})
+	i, err := ReadUvarint(varint)
+	if err != ErrNotMinimal {
+		t.Error("expected an error")
+	}
+	if i != 0 {
+		t.Error("expected i = 0")
+	}
+	varint = bytes.NewBuffer([]byte{0x81, 0x00})
+	i, err = binary.ReadUvarint(varint)
+	if i != 1 {
+		t.Error("expected varint 1")
 	}
 }
 
